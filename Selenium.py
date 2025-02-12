99509a227bd284a51bfee2274bb16c72a1fb6d98
eleven sheep <63571464+elevensheep@users.noreply.github.com>
2025-02-13 03:24:05 +0900

    Delete Selenium.py

--- Selenium.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
-
-browser = webdriver.Chrome("C:/chromedriver/chromedriver.exe")
-browser.get("https://naver.com")
-
-
-
