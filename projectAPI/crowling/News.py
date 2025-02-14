4d99781c9528746b69b8bbfa43576e8a59c9faf1
elevensheep <fttc100@gmail.com>
2025-02-14 16:35:14 +0900

    update news

--- projectAPI/crowling/News.py
+++ projectAPI/crowling/News.py
@@ -25,6 +25,19 @@ class News:
         
         return texts
     
+    # 신문사 : 중앙, 조선, 국민, ytn, 동아, 디지털타임즈, jtbc, mbc, 뉴시스, 서울신문
+
+    
+    def joongang(self, pageNum):
+        pass
+    def chosun(self, pageNum):
+        pass
+    def kmlb(self, pageNum):
+        pass
+    def ytn(self, pageNum):
+        pass
+    def donga(self, pageNum):
+        pass
     def digitalTimes(self, pageNum):
         # section_num : 정치 : 2400, 경제 : 3200, 산업 : 2200, ict : 2000, 금융 : 3100, 부동산 : 3500, 국제 : 2300, 사회 : 3700
         categorys = [2400, 3200, 2200, 2000, 3100, 3500, 2300, 3700]
@@ -38,6 +51,14 @@ class News:
             results[category] = texts 
         
         return results
-
+    def jtbc(self, pageNum):
+        pass
+    def mbc(self, pageNum):
+        pass
+    def newsis(self, pageNum):
+        pass
+    def seoul(self, pageNum):
+        pass
+    
 
 
